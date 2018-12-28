18d499165ae05513bf4c35aecb0307c0fdf90469
mmxw11 <mmxw11@users.noreply.github.com>
2018-12-28 12:50:25 +0200

    Link discord library dynamically.

--- DiscordRPConsoleClient/DiscordHandler.cpp
+++ DiscordRPConsoleClient/DiscordHandler.cpp
@@ -1,7 +1,15 @@
 #include "pch.h"
 
 #include "DiscordHandler.h"
+#include "discord-rpc/discord_rpc.h"
 #include "Windows.h"
+
+// link discord library
+#if defined(_WIN64)
+#pragma comment(lib, "discord-rpc-win64.lib")
+#else
+#pragma comment(lib, "discord-rpc-win32.lib")
+#endif
 
 DiscordHandler::DiscordHandler() {
     OutputDebugString(L"DiscordHandler constructor\n");
@@ -20,5 +28,6 @@ void DiscordHandler::shutdown() {
         // return;
     }
     OutputDebugString(L"Discord RP Client shutdown!\n");
+    Discord_Shutdown();
     this->initialized = false;
 }
