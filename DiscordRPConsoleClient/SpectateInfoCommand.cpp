b9f5030c4241718f7ad2ad8f5c06c5552ff43e93
mmxw11 <mmxw11@users.noreply.github.com>
2019-01-10 21:28:49 +0200

    Improve spectateinfo command

--- DiscordRPConsoleClient/SpectateInfoCommand.cpp
+++ DiscordRPConsoleClient/SpectateInfoCommand.cpp
@@ -6,18 +6,14 @@
 
 void SpectateInfoCommand::executeCommand(std::string* args, unsigned argsLength) {
     std::string& visibility = args[0];
-    if (visibility.empty()) {
-        std::cout << "Does an empty string look like a visibility settings to you?\nUse \"show\" or \"hide\"." << std::endl;
+    if (!sutils::equalsIgnoreCase("show", visibility) && !sutils::equalsIgnoreCase("hide", visibility)) {
+        std::cout << "Not a valid visibility setting! Use \"show\" or \"hide\"." << std::endl;
         return;
     }
     DiscordHandler& dhandler = DiscordHandler::getInstance();
     bool show = sutils::equalsIgnoreCase(visibility, "show");
     bool updated = dhandler.setSpectateInfo(show, true);
-    if (show) {
-        std::cout << "Spectate visibility set to show." << std::endl;
-    } else {
-        std::cout << "Spectate visibility set to hide." << std::endl;
-    }
+    std::cout << "Spectate visibility set to \"" << (show ? "show" : "hide") << "\"." << std::endl;
     if (!updated) {
         dhandler.printNotConnectedErrorMessage();
     }
